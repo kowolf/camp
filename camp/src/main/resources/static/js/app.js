ee8dc33679649931c39837a120600565cab1b683
Karl Wolf <karlwendy2005@gmail.com>
2015-08-20 22:28:15 -0600

    Fix UI.Select
    CSS and JS includes

--- camp/src/main/resources/static/js/app.js
+++ camp/src/main/resources/static/js/app.js
@@ -1,3 +1,3 @@
 // create our angular app and inject ngAnimate and ui-router 
 // =============================================================================
-angular.module('campApp', [ 'ngAnimate', 'ui.router','ui.grid','ngSanitize','ui.select','google-maps' ])
+angular.module('campApp', [ 'ngAnimate', 'ui.router','ui.grid','ui.select','ngSanitize','ui.select','google-maps' ])
